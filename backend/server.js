222acb6c706071f418fc0eab12dc1f144d68fc77
john2408 <john.torres.consulting@gmail.com>
2022-04-18 00:53:38 +0200

    working build

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,14 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
+
 // Server static assets if in production
 if(process.env.NODE_ENV === 'production'){
   //Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static(path.resolve(__dirname,'../frontend/build')));
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
-  })
+  });
 }else{
   app.get("/", (req, res) => {
     res.json({ message: "API running..." });
