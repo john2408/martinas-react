df7072d5219dcbbc1282a00f89522f99f0bba433
john2408 <john.torres.consulting@gmail.com>
2022-04-17 17:14:38 +0200

    working website

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -2,28 +2,25 @@ import './Banner.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+
+// Ref: http://react-responsive-carousel.js.org/storybook/?path=/story/01-basic--base
 export const Banner = () => {
   return (
-    <Carousel autoPlay dynamicHeight={true} width="100%">
+    <Carousel autoPlay dynamicHeight={true} width="100%" showThumbs={false} infiniteLoop= {true} showStatus= {false}>
         <div>
             <img alt="" src="images/1.jpg" className='banner__img' />
-            <p className="legend">Legend 1</p>  
         </div>
         <div>
             <img alt="" src="images/2.jpg" className='banner__img'/>
-            <p className="legend">Legend 2</p>
         </div>
         <div>
             <img alt="" src="images/3.jpg" className='banner__img'/>
-            <p className="legend">Legend 2</p>
         </div>
         <div>
             <img alt="" src="images/4.jpg" className='banner__img'/>
-            <p className="legend">Legend 2</p>
         </div>
         <div>
             <img alt="" src="images/5.jpg" className='banner__img'/>
-            <p className="legend">Legend 2</p>
         </div>
         
     </Carousel>
@@ -31,3 +28,5 @@ export const Banner = () => {
 }
 
 export default Banner;
+
+//<p className="banner__legend">Legend 1</p>  
